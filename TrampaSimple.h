b2e03a199bc15089af5cba9a9aae553a4be02c42
Carlos matias villarroel tarraga <villarroeltarragacarlosmatias2@gmail.com>
2025-06-12 15:46:11 -0400

    Create TrampaSimple.h

---
+++ TrampaSimple.h
@@ -0,0 +1,25 @@
+// TrampaSimple.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrampaComponent.h"
+#include "TrampaSimple.generated.h"
+
+UCLASS()
+class BOMBERMAN_012025_API ATrampaSimple : public ATrampaComponent
+{
+    GENERATED_BODY()
+    
+public:
+    ATrampaSimple();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void ActivarTrampa() override;
+
+private:
+    UPROPERTY(VisibleAnywhere)
+    UStaticMeshComponent* Mesh;
+};

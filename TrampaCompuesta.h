90e13fdb92fbe4d20a57475572b86d24c1d397f4
Carlos matias villarroel tarraga <villarroeltarragacarlosmatias2@gmail.com>
2025-06-12 15:46:57 -0400

    Create TrampaCompuesta.h

---
+++ TrampaCompuesta.h
@@ -0,0 +1,21 @@
+// TrampaCompuesta.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrampaComponent.h"
+#include "TrampaCompuesta.generated.h"
+
+UCLASS()
+class BOMBERMAN_012025_API ATrampaCompuesta : public ATrampaComponent
+{
+    GENERATED_BODY()
+    
+public:
+    void AgregarTrampa(ATrampaComponent* Trampa);
+
+    virtual void ActivarTrampa() override;
+
+private:
+    UPROPERTY()
+    TArray<ATrampaComponent*> Trampas;
+};
